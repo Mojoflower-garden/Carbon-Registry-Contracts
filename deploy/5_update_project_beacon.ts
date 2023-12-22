9b337cb16ac96e629e907d5b7e4dd5e2a660699f
Skjaldbaka17 <skjaldbaka17@gmail.com>
2023-12-22 21:24:24 +0000

    updgraded project contract on mainnet

--- deploy/5_update_project_beacon.ts
+++ deploy/5_update_project_beacon.ts
@@ -6,11 +6,6 @@ const main: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { save } = deployments;
 
   console.log('UPDATE!');
-  // const OldProject = await ethers.getContractFactory('OldProject');
-  // await upgrades.forceImport(
-  //   '0x983a099b75bee66b265c23474917bb3e84a47708',
-  //   OldProject
-  // );
 
   const Project = await ethers.getContractFactory('Project');
 
@@ -18,13 +13,12 @@ const main: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     hre.network.name === 'mumbai'
       ? '0x983a099b75bee66b265c23474917bb3e84a47708'
       : hre.network.name === 'polygon'
-      ? ''
+      ? '0xb4A2E587B56d40e33395645c11c822bCC520E2ef'
       : '';
   const projectBeaconAddress = await upgrades.erc1967.getBeaconAddress(
     projectAddress
   );
   console.log('Project Beacon Address', projectBeaconAddress);
-  //   const projectBeaconAddress = '';
 
   const projectBeaconImplementationAddress =
     await upgrades.beacon.getImplementationAddress(projectBeaconAddress);
@@ -36,23 +30,6 @@ const main: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
   await upgrades.upgradeBeacon(projectBeaconAddress, Project);
   console.log('Beacon upgraded');
-
-  //   const projectBeaconImplementationArtifact =
-  //     await deployments.getExtendedArtifact('Project');
-  //   const projectBeaconArtifact = await deployments.getExtendedArtifact(
-  //     'IBeaconUpgradeable'
-  //   );
-
-  //   const time = Date.now();
-
-  //   await save(`${time}-Project_beacon`, {
-  //     address: projectBeaconAddress,
-  //     ...projectBeaconArtifact,
-  //   });
-  //   await save(`${time}-Project_implementation`, {
-  //     address: projectBeaconImplementationAddress,
-  //     ...projectBeaconImplementationArtifact,
-  //   });
 };
 export default main;
 main.tags = ['UpgradeProjectBeacon'];
