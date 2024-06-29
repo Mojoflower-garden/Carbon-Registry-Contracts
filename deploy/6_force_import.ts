f982d6847cf0e1968cb17892d8b8f86772a2ea88
Skjaldbaka17 <skjaldbaka17@gmail.com>
2024-06-29 19:16:57 +0100

    BATCH Transfers

--- deploy/6_force_import.ts
+++ deploy/6_force_import.ts
@@ -3,14 +3,14 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/dist/types';
 
 const main: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const OldProject = await ethers.getContractFactory('OldProject');
+  const Project = await ethers.getContractFactory('Project');
   await upgrades.forceImport(
     hre.network.name === 'baseSepolia'
       ? '0xa4686bcfa897d1cc56ec09b306d0c5a8096cb5aa'
       : hre.network.name === 'polygon'
       ? '0xb4A2E587B56d40e33395645c11c822bCC520E2ef'
       : '',
-    OldProject
+    Project
   );
 };
 export default main;
