344e9f7a06dcde130a60c15739f4bec684e2a8d7
Skjaldbaka17 <skjaldbaka17@gmail.com>
2024-06-29 19:32:38 +0100

    dep

--- deploy/7_update_project_beacon.ts
+++ deploy/7_update_project_beacon.ts
@@ -6,14 +6,17 @@ const main: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   console.log('7');
   const { save } = deployments;
   const Project = await ethers.getContractFactory('Project');
+  const isProdTest = false;
 
   const isMainnet = hre.network.name === 'polygon';
-  const projectAddress =
-    hre.network.name === 'baseSepolia'
-      ? '0xa4686bcfa897d1cc56ec09b306d0c5a8096cb5aa'
-      : hre.network.name === 'polygon'
-      ? '0xb4A2E587B56d40e33395645c11c822bCC520E2ef'
-      : '';
+  const projectAddress = isProdTest
+    ? // ? '0x0B0fCaCD2336A5f000661fF5E69aA70c28fD526D'
+      '0x225346387256b303ad8aee735b509b4282534382'
+    : hre.network.name === 'baseSepolia'
+    ? '0xa4686bcfa897d1cc56ec09b306d0c5a8096cb5aa'
+    : isMainnet
+    ? '0xb4A2E587B56d40e33395645c11c822bCC520E2ef'
+    : '';
   const projectBeaconAddress = await upgrades.erc1967.getBeaconAddress(
     projectAddress
   );
@@ -30,7 +33,6 @@ const main: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     'Project Beacon implementation deployed to:',
     projectBeaconImplementationAddress
   );
-
   const res = await upgrades.upgradeBeacon(projectBeaconAddress, Project, {
     txOverrides: {
       maxFeePerGas: ethers.parseUnits(isMainnet ? '200' : '2', 'gwei'),
